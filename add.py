7433940b4d1e75ba07a0c21123d5a74aa1ba557a
Guðjón Karl Ólafsson <gudjono19@ru.is>
2020-03-27 09:33:57 +0000

    I was not completly sure how the testing raise method worked but I return a string that is constant with the outcome of the test sheat

--- add.py
+++ add.py
@@ -3,12 +3,18 @@ def Add(numbers):
         return 0
     else:
         total = 0
+        string = 'Negatives not allowed:'
         numbers = numbers.replace('\n', ',')
         numbers = numbers.split(',')
         for letter in numbers:
-            if letter.isdigit():
-                if int(letter)< 1000:
-                    total += int(letter)
+            if int(letter)< 1000:
+                if int(letter) < 0:
+                    if string != 'Negatives not allowed:':
+                        string+= ','
+                    string += letter
+                total += int(letter)
+        if string != 'Negatives not allowed:':
+            return string
         return total
 
 assert Add('') == 0
@@ -18,3 +24,4 @@ assert Add('1,2,3,4,5') == 15
 assert Add('10,2,5,22,1,1') == 41
 assert Add('1\n2,3') == 6
 assert Add('1001,2') == 2
+assert Add('-1,2') == 'Negatives not allowed:-1'

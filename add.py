24df0af9e938389a50d8d7ffdf974292df240fc0
Guðjón Karl Ólafsson <gudjono19@ru.is>
2020-03-27 09:18:10 +0000

    we now can add together an unspecific number of integers together

--- add.py
+++ add.py
@@ -2,11 +2,15 @@ def Add(numbers):
     if numbers == '':
         return 0
     else:
-        if len(numbers) == 3:
-            numbers = numbers.split(',')
-            return int(numbers[0])+int(numbers[1])
-        return int(numbers)
+        total = 0
+        numbers = numbers.split(',')
+        for letter in numbers:
+            if letter.isdigit():
+                total += int(letter)
+        return total
 
 assert Add('') == 0
 assert Add('1') == 1
 assert Add('1,2') == 3
+assert Add('1,2,3,4,5') == 15
+assert Add('10,2,5,22,1,1') == 41

0d1b2f70b1e31cf79962258a68b5e45f02c8af0b
Guðjón Karl Ólafsson <gudjono19@ru.is>
2020-03-27 09:39:23 +0000

    we can now bypass any muck in the string and only add integers together

--- add.py
+++ add.py
@@ -2,10 +2,21 @@ def Add(numbers):
     if numbers == '':
         return 0
     else:
+        if len(numbers) == 1:
+            return int(numbers)
         total = 0
         string = 'Negatives not allowed:'
-        numbers = numbers.replace('\n', ',')
-        numbers = numbers.split(',')
+        clean_string = ''
+        while numbers != '':
+            if numbers[0].isdigit() or numbers[0] == '-' and numbers[1].isdigit():
+                clean_string += numbers[0]
+            else:
+                if clean_string == '' or clean_string[-1] == ',' or len(numbers) == 1:
+                    pass
+                else:
+                    clean_string += ','
+            numbers = numbers[1:]
+        numbers = clean_string.split(',')
         for letter in numbers:
             if int(letter)< 1000:
                 if int(letter) < 0:
@@ -25,3 +36,6 @@ assert Add('10,2,5,22,1,1') == 41
 assert Add('1\n2,3') == 6
 assert Add('1001,2') == 2
 assert Add('-1,2') == 'Negatives not allowed:-1'
+assert Add("2,-4,3,-5") == "Negatives not allowed:-4,-5"
+assert Add("//X\n1X2") == 3
+assert Add("//%\n1%2%3") == 6

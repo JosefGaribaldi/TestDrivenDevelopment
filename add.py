b724a6b437acc26b460a222061cad7b1fdcaa427
Guðjón Karl Ólafsson <gudjono19@ru.is>
2020-03-27 09:24:35 +0000

    the method will now bypass any newdelimeter in the string

--- add.py
+++ add.py
@@ -3,6 +3,7 @@ def Add(numbers):
         return 0
     else:
         total = 0
+        numbers = numbers.replace('\n', ',')
         numbers = numbers.split(',')
         for letter in numbers:
             if letter.isdigit():
@@ -14,3 +15,4 @@ assert Add('1') == 1
 assert Add('1,2') == 3
 assert Add('1,2,3,4,5') == 15
 assert Add('10,2,5,22,1,1') == 41
+assert Add('1\n2,3') == 6

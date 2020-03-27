801a850d25c6eabc6166834d67250d26e940d422
Guðjón Karl Ólafsson <gudjono19@ru.is>
2020-03-27 09:26:40 +0000

    we now ignore all numbers larger then 999

--- add.py
+++ add.py
@@ -7,7 +7,8 @@ def Add(numbers):
         numbers = numbers.split(',')
         for letter in numbers:
             if letter.isdigit():
-                total += int(letter)
+                if int(letter)< 1000:
+                    total += int(letter)
         return total
 
 assert Add('') == 0
@@ -16,3 +17,4 @@ assert Add('1,2') == 3
 assert Add('1,2,3,4,5') == 15
 assert Add('10,2,5,22,1,1') == 41
 assert Add('1\n2,3') == 6
+assert Add('1001,2') == 2

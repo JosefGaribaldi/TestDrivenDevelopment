51c979626b7f245b40f346b7099614e355a47edd
Guðjón Karl Ólafsson <gudjono19@ru.is>
2020-03-27 09:13:52 +0000

    we now have a way of adding two numbers together

--- add.py
+++ add.py
@@ -2,8 +2,11 @@ def Add(numbers):
     if numbers == '':
         return 0
     else:
-        if numbers.isdigit():
-            return int(numbers)
+        if len(numbers) == 3:
+            numbers = numbers.split(',')
+            return int(numbers[0])+int(numbers[1])
+        return int(numbers)
 
 assert Add('') == 0
 assert Add('1') == 1
+assert Add('1,2') == 3
